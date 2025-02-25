3f0de5ec674d8d33b08dadb48413dce49d3b2ffb
Tushar Dubey <59757106+tushar-dubey5@users.noreply.github.com>
2025-02-26 00:36:28 +0530

    Update CalculatorTest.java

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -6,7 +6,7 @@ public class CalculatorTest {
 
     @Test
     public void testAdd() {
-        assertEquals(5.0, Calculator.add(2, 3), 0.0001);
+        assertEquals(6.0, Calculator.add(2, 4), 0.0001);
         assertEquals(-1.0, Calculator.add(-2, 1), 0.0001);
         assertEquals(0.0, Calculator.add(0, 0), 0.0001);
     }
